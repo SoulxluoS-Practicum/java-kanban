a280c0efd7bf13b821e8cfb967cb68632c5cce64
SoulxluoS <70120313+YoungSoulluoS@users.noreply.github.com>
2025-10-05 07:04:01 +0500

    ТЗ-6 - Добавить Id задачи в сообщение теста

--- test/managers/history/InMemoryHistoryManagerTest.java
+++ test/managers/history/InMemoryHistoryManagerTest.java
@@ -21,15 +21,14 @@ class InMemoryHistoryManagerTest {
     void add() {
         Task task1 = new Task("Task-1 Name", "Task-1 Description");
         historyManager.add(task1);
-        assertFalse(historyManager.getHistory().isEmpty(), "Задача не добавлена в HistoryManager");
+        assertFalse(historyManager.getHistory().isEmpty(), "Задача %s не добавлена в HistoryManager".formatted(task1.getId()));
     }
 
     @Test
     void remove() {
-        Task task1 = new Task("Task-1 Name", "Task-1 Description");
-        historyManager.add(task1);
-        assertFalse(historyManager.getHistory().isEmpty(), "Задача не добавлена в HistoryManager");
-        historyManager.remove(task1.getId());
-        assertTrue(historyManager.getHistory().isEmpty(), "Задача не удалена из HistoryManager");
+        Task task2 = new Task("Task-2 Name", "Task-2 Description");
+        historyManager.add(task2);
+        historyManager.remove(task2.getId());
+        assertTrue(historyManager.getHistory().isEmpty(), "Задача %s не удалена из HistoryManager".formatted(task2.getId()));
     }
 }
